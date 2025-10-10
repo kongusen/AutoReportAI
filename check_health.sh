4001a00ab273e837ae03930b7e364e99b9e155ba
448486810 <32874034+448486810@users.noreply.github.com>
2025-10-10 15:26:30 +0800

    chore: 提交当前工作区变更

--- check_health.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-TOKEN=$(cat /Users/shan/work/uploads/AutoReportAI/login.json | jq -r '.data.access_token')
-curl -s "http://localhost:8000/api/v1/health" -H "Authorization: Bearer $TOKEN" | jq -r '.status // "DOWN"'
